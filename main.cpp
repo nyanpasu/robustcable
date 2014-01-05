c8acabd1c2e0e1c046153941e2f5e46859ff7e04
nyanpasu <nyanpasugit@gmail.com>
2014-01-05 20:13:07 +0800

    Updated to SDL 2 and finally knew what I was doing.

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
  *
  *       Filename:  main.cpp
  *
- *    Description:  Help plans out wire art for the game known as SS13
+ *    Description:  
  *
  *        Version:  1.0
- *        Created:  01/04/2014 08:21:45 PM
+ *        Created:  01/05/2014 06:23:16 PM
  *       Revision:  none
  *       Compiler:  gcc
  *
@@ -14,93 +14,67 @@
  *   Organization:  
  *
  * =====================================================================================
+         */
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+const int TOTAL_WIRE_SPRITES_ACROSS = 9;
+const int TOTAL_WIRE_SPRITES_DOWN   = 8;
+const int TOTAL_WIRE_SPRITES        = TOTAL_WIRE_SPRITES_ACROSS * TOTAL_WIRE_SPRITES_DOWN;
+
+const std::string SPRITE_DIR = "images/";
+
+/**
+ * Makes a lot of noise when something goes wrong.
  */
-#include <stdlib.h>
-#include <SDL/SDL.h>
-
-const int SPRITE_WIDTH = 32;
-const int SPRITE_HEIGHT = 32;
-const int TOTAL_SPRITES_ACROSS = 9;
-const int TOTAL_SPRITES_DOWN = 8;
-const int TOTAL_SPRITE = TOTAL_SPRITES_ACROSS * TOTAL_SPRITES_DOWN;
-
-int SPRITE_DIR = "images/";
-
-class Button
+void logSDLError( const std::string &message ) 
 {
-        private:
-                SDL_Rect box;
-
-        public:
-                Button ( int x, int y, int w, int h);
-
-                void handle_events();
-                void show();
-};
-
-Button::Button ( int x, int y, int w, int h)
-{
-        box.x = x;
-        box.y = y;
-        box.w = w;
-        box.h = h;
+        std::cout << "Error: " << message << SDL_GetError() << std::endl;
 }
 
-void Button::handle_events()
+/**
+ * Loads a BMP image into a texture, then onto the rendering device
+ * TODO: Add PNG support
+ * @param file The BMP image to load
+ * @param renderer The renderer to load the texture onto
+ * @return the loaded texture, or nullptr if something went wrong.
+ */
+SDL_Texture *loadTexture( const std::string &file, SDL_Renderer *renderer )
 {
-        int x = 0, y = 0;
+        SDL_Texture *texture = IMG_LoadTexture( renderer, file.c_str() );
 
-        if ( event.type == SDL_MOUSEMOTION )
+        if ( texture == nullptr )
         {
-                x = event.motion.x;
-                y = event.motion.y;
-
-                // If the mouse is over the button
-                if ( ( x > box.x ) && ( x < box.x + box.w) && ( y > box. y) && ( y < box.y + box.h) )
-                {
-                        // Enlarge the box.
-                }
-                else
-                {
-                        // Set it to the regular size.
-                }
+               logSDLError("Texture at loadTexture()");
         }
 
-        if ( event.type == SDL_MOUSEBUTTONDOWN )
-        {
-                if (event.button.button == SDL_BUTTON_LEFT )
-                {
-                        // Change the button to that of the corresponding sprite
-                }
-        }
+        return texture;
 }
 
-void Button::show()
+/**
+ * Draw an SDL_Texture onto an SDL_Renderer at a selected postion
+ * @param texture The source texture
+ * @param renderer The destination renderer
+ * @param x x coordinate
+ * @param y y coordinate
+ * @param w width of texture
+ * @param h height of texture
+ */
+void renderTexture( SDL_Texture *texture, SDL_Renderer *renderer, int x, int y, int w, int h )
 {
-        // Display the button in its current state.
+        SDL_Rect destination;
+        destination.x = x;
+        destination.y = y;
+        destination.w = w;
+        destination.h = h;
+
+        SDL_RenderCopy( renderer, texture, NULL, &destination );
 }
 
-void cut_surface ( int x, int y, SDL_Surface *source, SDL_Surface *destination )
-{
-        SDL_Rect offset;
-
-        offset.x = x;
-        offset.y = y;
-
-        SDL_BlitSurface ( source, clip, destination, &offset );
-}
-
-void load_sprites_from_spritesheet ( SDL_Surface *spritesheet, SDL_Surface *spritearray ) // TODO Complete the load_sprites function
-{
-        // 
-        // For every sprite across
-        // and every sprite below
-        // Clip it according to the sprite width and height
-        // and store it into the sprite array.
-        //
-}
-
-// Maybe a function to set the clips?
 
 int main(int argc, const char *argv[])
 {
@@ -127,5 +101,67 @@ int main(int argc, const char *argv[])
         // choose the most appropriate sprite
         // TODO: Load image in background.
         // 
+
+        // Start SDL
+        if ( SDL_Init(SDL_INIT_EVERYTHING) != 0 )
+        {
+                logSDLError("SDL");
+                return 1;
+        }
+
+        // Start SDL and the main renderer
+        SDL_Window *window = nullptr;
+        window = SDL_CreateWindow( "New window", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+
+        if ( window == nullptr )
+        {
+                logSDLError("Window");
+                return 1;
+        }
+
+        SDL_Renderer *mainRenderer = nullptr;
+        mainRenderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+
+        if ( mainRenderer == nullptr )
+        {
+                logSDLError("Renderer");
+                return 1;
+        }
+
+        // Load the all the sprites in images
+        // for each *.png images in SPRITES_DIR
+        // load image into spritesheets[]
+        //
+        // Currently place holder that will only load green cables.
+        SDL_Texture *spriteSheet = loadTexture( "./image/power_cond_green.bmp", mainRenderer);
+
+        // And check every file to see if it loaded successfully
+        //
+        // Currently just checking the only spritesheet loaded
+        if ( spriteSheet == nullptr )
+        {
+               logSDLError("Failed to read texture");
+        }
+        
+        // Do teh render routine
+        // TODO have to write a render function that does the following:
+        // - Look at the state of the canvas
+        // - Draw the canvas
+        // - Draw the current "pen" sprite
+        // - Draw the menu if it's enabled
+        //
+        // Currently just viewing the spritesheet.
+        SDL_RenderClear( mainRenderer );
+        renderTexture( spriteSheet, mainRenderer, 0, 0 , SCREEN_WIDTH, SCREEN_HEIGHT);
+
+        SDL_RenderPresent( mainRenderer );
+        SDL_Delay(2000);
+
+        SDL_DestroyTexture(spriteSheet);
+        SDL_DestroyRenderer(mainRenderer);
+        SDL_DestroyWindow(window);
+
+        SDL_Quit();
+
         return 0;
 }
